57e05b7f1b2622efd3dc3c174e71168b6dbc64d8
6felix <felix.okoronkwo.1@gmail.com>
2023-05-30 20:06:19 +0100

    implemented api blueprint

---
+++ api/v1/app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+App instance of
+flask application
+"""
+
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def refresh_storage(self):
+    """closes query after each request
+    making new request start with a new query
+    to a possibly updated database"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host=getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=getenv('HBNB_API_PORT', '5000'),
+            threaded=True)
