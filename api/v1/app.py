019f576570252a9692d1ebe74f8049242367cf8e
6felix <felix.okoronkwo.1@gmail.com>
2023-05-30 21:06:40 +0100

    added custom 404 response

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,6 +8,7 @@ from flask import Flask
 from models import storage
 from api.v1.views import app_views
 from os import getenv
+from flask import jsonify
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -19,6 +20,13 @@ def refresh_storage(self):
     making new request start with a new query
     to a possibly updated database"""
     storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    """handler for 404 errors that returns
+    a JSON-formatted 404 status code response
+    """
+    return jsonify({'error': 'Not found'}), 404
 
 
 if __name__ == "__main__":
