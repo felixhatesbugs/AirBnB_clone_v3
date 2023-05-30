583a7ebc3a008fc437507c29e170ad685078f36e
6felix <felix.okoronkwo.1@gmail.com>
2023-05-30 09:49:07 +0100

    updated storage engines with count and get methods

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,18 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """Returns the object based on the 
+        class and its ID, or None if not found"""
+        storage = self.all(cls)
+        for obj in storage.values():
+            if obj.id == id:
+                return obj
+            else:
+                None
+
+    def count(self, cls=None):
+        """Returns the number of objects in
+        storage matching the given class"""
+        return len(self.all(cls))
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,18 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """Returns the object based on the 
+        class and its ID, or None if not found"""
+        storage = self.all(cls)
+        for obj in storage.values():
+            if obj.id == id:
+                return obj
+            else:
+                None
+
+    def count(self, cls=None):
+        """Returns the number of objects in
+        storage matching the given class"""
+        return len(self.all(cls))
