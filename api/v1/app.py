3fd8f2e6161d508a9c13f5abfa2871d06a6cb20b
6felix <felix.okoronkwo.1@gmail.com>
2023-05-31 22:04:01 +0100

    added Cross Origin Resource Sharing feature

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,8 +9,10 @@ from models import storage
 from api.v1.views import app_views
 from os import getenv
 from flask import jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, origins='0.0.0.0')
 app.register_blueprint(app_views)
 
 
